4e480023a6a1965a71c394cc4d0c37d73761f0ce
Lynne Jones <lynnej@uw.edu>
2016-02-08 16:45:39 -0800

    Added test for badly formatted input catalog.

--- tests/testOrbits.py
+++ tests/testOrbits.py
@@ -1,5 +1,5 @@
 import unittest
-from lsst.sims.movingObjects.orbits import Orbits
+from lsst.sims.movingObjects import Orbits
 
 class testOrbits(unittest.TestCase):
     def testReadOrbits(self):
@@ -8,7 +8,8 @@ class testOrbits(unittest.TestCase):
         self.assertEqual(orbits.nSso, 4)
         orbits.readOrbits('test_orbitsA.des')
         self.assertEqual(orbits.nSso, 4)
+        with self.assertRaises(ValueError):
+            orbits.readOrbits('test_orbitsBad.des')
 
 if __name__ == '__main__':
     unittest.main()
-
