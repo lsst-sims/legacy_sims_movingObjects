d5300eb5d1a650e10326a51315353a83cae5dfaf
danielsf <scott.f.daniel@gmail.com>
2016-05-10 13:30:35 -0700

    unit test to check slicing and iteration on orbit subset

--- tests/testOrbits.py
+++ tests/testOrbits.py
@@ -62,6 +62,37 @@ class TestOrbits(unittest.TestCase):
         self.assertEqual(orbit_slice[2], orbits[5])
         self.assertEqual(len(orbit_slice), 3)
 
+
+    def testOffsetDatafram(self):
+        """
+        Test that we can slice and iterate through an orbits
+        dataframe that has already been sub-selected from another
+        datafram
+        """
+        orbits0 = Orbits()
+        orbits0.readOrbits(os.path.join(self.testdir, 'test_orbitsNEO.s3m'), skiprows=1)
+
+        orbitsSub = Orbits()
+        orbitsSub.setOrbits(orbits0.orbits.query('index>1'))
+
+        self.assertEqual(len(orbitsSub), 6)
+
+        orbit_slice = orbitsSub[2:6]
+        self.assertEqual(orbit_slice[0], orbitsSub[2])
+        self.assertEqual(orbit_slice[1], orbitsSub[3])
+        self.assertEqual(orbit_slice[2], orbitsSub[4])
+        self.assertEqual(orbit_slice[3], orbitsSub[5])
+        self.assertEqual(len(orbit_slice), 4)
+
+        orbit_slice = orbitsSub[1:5:2]
+        self.assertEqual(orbit_slice[0], orbitsSub[1])
+        self.assertEqual(orbit_slice[1], orbitsSub[3])
+        self.assertEqual(len(orbit_slice), 2)
+
+        for ii, oo in enumerate(orbitsSub):
+            self.assertEqual(oo, orbits0[ii+2])
+
+
     def testReadOrbits(self):
         orbits = Orbits()
         orbits.readOrbits(os.path.join(self.testdir, 'test_orbitsQ.des'))
