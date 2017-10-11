84520759f4e4486622372e304ec352d6124873ec
Lynne Jones <lynnej@uw.edu>
2017-10-11 15:27:42 -0700

    Removed module that doesn't exist

--- python/lsst/sims/movingObjects/__init__.py
+++ python/lsst/sims/movingObjects/__init__.py
@@ -25,7 +25,6 @@
 from .version import *
 from .orbits import *
 from .ooephemerides import *
-from .adamephemerides import *
 from .chebyFits import *
 from .chebyValues import *
 from .chebyshevUtils import *
